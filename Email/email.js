3707d2fdd84db1ba35e7faa49afd0de16e77bc15
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-03-07 10:48:36 +0530

    login done

--- Email/email.js
+++ Email/email.js
@@ -1,9 +1,22 @@
-let email=document.getElementById('email')
-let password=document.getElementById('password')
-let login=document.getElementById('login-btn')
+let email=document.getElementById('ln-email')
+let password=document.getElementById('ln-password')
+let login_btn=document.getElementById('login-btn')
 let map=new Map()
-login.addEventListener('click',function(){
-if(!map.has(email.value)){
-    map.set(email.value,password.value)
+let arr=[]
+map.set('hulk@gmail.com','hulk@123')
+map.set('thor@gmail.com','thor@123')
+map.set('ironman@gamil.com','ironman@123')
+
+login_btn.addEventListener('click',function(){
+    
+if(map.has(email.value)){
+    // arr.push(email.value)
+    if(map.get(email.value)==password.value){
+        // arr.push(email.value)
+        let login_container=document.querySelector('.login-container')
+        login_container.style.display='none'
+        let home_page_container=document.querySelector(".home-page-container")
+        home_page_container.style.display='flex'
+    }
 }
 })
